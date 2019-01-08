bbfdb690c4d46f52dd74213846facf9e14af9216
Sergey Kotkov <kotkovdev@gmail.com>
2019-01-08 17:24:49 +0500

    Login added

--- dbstructure.sql
+++ dbstructure.sql
@@ -49,6 +49,8 @@ CREATE TABLE `crw_users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `reset_hash` varchar(255) NOT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- src/routes.php
+++ src/routes.php
@@ -3,36 +3,45 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-/*
- * GET
- */
+if (!isset($_SESSION['id'])) {
+    $app->any('/', App\Controllers\UserController::class);
+    $app->get('/login', \App\Controllers\UserController::class);
+    $app->get('/forgot', '\App\Controllers\UserController:forgot');
+    $app->post('/login', '\App\Controllers\UserController:login');
+    $app->post('/forgot', '\App\Controllers\UserController:forgot');
+} else {
 
-$app->any('/', App\Controllers\AnalyzeController::class);
-$app->get('/download', App\Controllers\SaveController::class);
-$app->get('/jobs', App\Controllers\JobsController::class);
-$app->get('/jobs/log', 'App\Controllers\JobsController:log');
-$app->get('/login', \App\Controllers\UserController::class);
-$app->get('/forgot', '\App\Controllers\UserController:forgot');
-$app->get('/instlist', function ($req, $res, $args){
-    global $container;
-    $instances = new \App\Controllers\InstancesController($container->get('db')->table('instances'));
-    return $instances->__invoke($req, $res);
-});
-$app->get('/process', function($req, $res, $args){
-    global $container;
-    $processController = new App\Controllers\ProcessController($container->get('db')->table('queue'), $container->get('db')->table('instances'));
-    return $processController->process();
-});
-/*
- * POST
- */
-$app->post('/process', App\Controllers\ProcessController::class);
-$app->post('/login', '\App\Controllers\UserController:login');
-$app->post('/save', 'App\Controllers\AnalyzeController:save');
-$app->post('/download/save', 'App\Controllers\SaveController:save');
-$app->post('/jobs/remove', 'App\Controllers\JobsController:remove');
-$app->post('/instlist/remove', function ($req, $res, $args){
-    global $container;
-    $instances = new \App\Controllers\InstancesController($container->get('db')->table('instances'));
-    return $instances->remove($req, $res);
-});
+    /*
+     * GET
+     */
+    $app->any('/', App\Controllers\AnalyzeController::class);
+    $app->get('/download', App\Controllers\SaveController::class);
+    $app->get('/jobs', App\Controllers\JobsController::class);
+    $app->get('/jobs/log', 'App\Controllers\JobsController:log');
+    $app->get('/login', \App\Controllers\UserController::class);
+    $app->get('/forgot', '\App\Controllers\UserController:forgot');
+    $app->get('/instlist', function ($req, $res, $args){
+        global $container;
+        $instances = new \App\Controllers\InstancesController($container->get('db')->table('instances'));
+        return $instances->__invoke($req, $res);
+    });
+    $app->get('/process', function($req, $res, $args){
+        global $container;
+        $processController = new App\Controllers\ProcessController($container->get('db')->table('queue'), $container->get('db')->table('instances'));
+        return $processController->process();
+    });
+    /*
+     * POST
+     */
+    $app->post('/process', App\Controllers\ProcessController::class);
+    $app->post('/login', '\App\Controllers\UserController:login');
+    $app->post('/forgot', '\App\Controllers\UserController:forgot');
+    $app->post('/save', 'App\Controllers\AnalyzeController:save');
+    $app->post('/download/save', 'App\Controllers\SaveController:save');
+    $app->post('/jobs/remove', 'App\Controllers\JobsController:remove');
+    $app->post('/instlist/remove', function ($req, $res, $args){
+        global $container;
+        $instances = new \App\Controllers\InstancesController($container->get('db')->table('instances'));
+        return $instances->remove($req, $res);
+    });
+}
